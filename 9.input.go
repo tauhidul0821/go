c369791bf22fc05ae8219313a53bf14fd23c7b88
Md. Tauhidul Islam <tauhidul.islam@brainstation23.com>
2021-08-03 13:53:49 +0600

    now learn how to input a vallue

--- 9.input.go
+++ 9.input.go
@@ -2,5 +2,22 @@ package main
 import ("fmt")
 
 func main(){
-	fmt.Print("type a number")
+	var i int
+	var j string
+
+
+	fmt.Print("What is your name : ")
+	fmt.Scan(&j)
+
+	fmt.Println()
+
+
+	fmt.Print("What is your age: ")
+
+	fmt.Scan(&i)
+
+	fmt.Println()
+
+	fmt.Println("your name ", j,"and you are", i ,"Years old")
+
 }
